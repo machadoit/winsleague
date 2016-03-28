2bf99bff2848d5c56a866e214f8061bccdc05ce3
Noah Spitzer-Williams <noah@spitzer-williams.com>
2016-03-27 21:28:23 -0700

    update team summary after creating new pool team

--- app/server/collections/hooks/pool_teams.js
+++ app/server/collections/hooks/pool_teams.js
@@ -1,3 +1,7 @@
 PoolTeams.before.remove((userId, doc) => {
   PoolTeamPicks.remove({ poolTeamId: doc._id });
 });
+
+PoolTeams.after.insert((userId, doc) => {
+  Modules.server.poolTeams.updateTeamSummary(doc._id);
+});
